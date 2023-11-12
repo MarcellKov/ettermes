9fc79fbd8ef14e0914327e6e6a196512ffac011b
MarcellKov <kmarcell379@gmail.com>
2023-11-12 22:45:04 +0100

    SQL_Server

--- DB/DBC.cs
+++ DB/DBC.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.Configuration;
 using webapp.DataModels;
 
 namespace webapp.DB
 {
     public class DBC : DbContext
     {
+        public IConfiguration _config;
+        public DBC(IConfiguration config)
+
+        {
+            _config = config;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseInMemoryDatabase(databaseName: "Userdb");
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
         }
         public DbSet<UserDataModel> UserDatas { get; set; }
     }
---
+++ Migrations/20231112212817_Add_Users_Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Users_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserDatas",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    uname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDatas", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDatas");
+        }
+    }
+}
